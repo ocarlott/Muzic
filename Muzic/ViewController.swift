3016c8c976464976e2b0cf6758046248169ca609
Michael Ngo <michaelngo@mivsolution.com>
2017-01-16 18:37:11 -0600

    Initial Commit

---
+++ Muzic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Muzic
+//
+//  Created by Michael Ngo on 1/16/17.
+//  Copyright © 2017 MIV Solution. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
